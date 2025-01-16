0dd18648be671c2a7790035d67f9117bc37eb5c7
Ritik Raushan <94674369+Ritikraushan1@users.noreply.github.com>
2025-01-16 23:27:22 +0530

    Update auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -147,7 +147,7 @@ def signin():
     token_data = {
         'user_id': user.id,
         'role': user.role,
-        'exp': datetime.utcnow() + timedelta(seconds=current_app.config['JWT_EXPIRATION_DELTA'])
+        'exp': (datetime.utcnow() + timedelta(days=365)).timestamp()
     }
     token = jwt.encode(token_data, current_app.config['JWT_SECRET_KEY'], algorithm='HS256')
 
